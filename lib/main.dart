a1c5cbe64b7f8270eed0d156066aadce5d9b5a31
Murigi-Nganga <murigigracious@students.uonbi.ac.ke>
2024-01-12 09:22:11 +0300

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(const App());
+}
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'Biometric attendance application',
+      // theme: appTheme,
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+      // routes: appRoutes,
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text("This is the Home Page")
+      ),
+    );
+  }
+}
